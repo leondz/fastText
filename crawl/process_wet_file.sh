f278026e42bdbcaee98497c1cb55c3f733befddb
Leon Derczynski <ld@itu.dk>
2019-06-27 15:54:07 +0200

    account for label + probability prefix
    If minimum text length is 100 chars then we should account for the label here, which eats the first 13-14 characters of length (depending on language code) and then the 9 characters given to the probability (and space)

--- crawl/process_wet_file.sh
+++ crawl/process_wet_file.sh
@@ -23,7 +23,7 @@ fastText/fasttext predict-prob lid.176.bin "tmp/${FILENAME}.warc.wet" > "tmp/${F
 
 #echo "Splitting ${FILENAME}.warc.wet per language"
 paste "tmp/${FILENAME}.lid" "tmp/${FILENAME}.warc.wet" | \
-    awk '($2 > 0.8 || ($1=="__label__hr" && $2 > 0.4)) && length() > 100 {lang = substr($1, 10); $1=""; $2=""; print $0 >> "shard/"lang".txt"}'
+    awk '($2 > 0.8 || ($1=="__label__hr" && $2 > 0.4)) && length() > 122 {lang = substr($1, 10); $1=""; $2=""; print $0 >> "shard/"lang".txt"}'
 
 #echo "Removing tmp files"
 rm "tmp/${FILENAME}.lid"
